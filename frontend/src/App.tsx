c6ac66281b2d9256511631de73bc3fe145b62d5a
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-03-15 16:42:13 -0500

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,1 +1,24 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Marketplace from './pages/Marketplace';
+import ProductDetail from './pages/ProductDetail';
+import UserDashboard from './pages/UserDashboard';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
+const App: React.FC = () => {
+    return (
+        <Router>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Marketplace />} />
+                <Route path="/product/:id" element={<ProductDetail />} />
+                <Route path="/dashboard" element={<UserDashboard />} />
+            </Routes>
+            <Footer />
+        </Router>
+    );
+};
+
+export default App;
 
