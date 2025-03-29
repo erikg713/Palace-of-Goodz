0d88a0d4f2e02cb5809b783b3a07ffbf300d4fe6
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-03-29 15:39:52 -0500

    Update db.ts

--- frontend/src/config/db.ts
+++ frontend/src/config/db.ts
@@ -1,11 +1,16 @@
-import mongoose from "mongoose";
+import { Pool } from "pg";
+
+const pool = new Pool({
+  connectionString: process.env.POSTGRES_URI,
+});
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI as string);
-    console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
+    const client = await pool.connect();
+    console.log(`✅ PostgreSQL Connected: ${client.host}`);
+    client.release();
   } catch (error) {
-    console.error(`❌ MongoDB Connection Error: ${error}`);
+    console.error(`❌ PostgreSQL Connection Error: ${error}`);
     process.exit(1);
   }
 };
