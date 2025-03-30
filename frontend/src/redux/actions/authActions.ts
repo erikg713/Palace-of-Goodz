47148f6e06378ca093a56e38240354ccc97f3933
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-03-29 23:35:31 -0500

    Update authActions.ts

--- frontend/src/redux/actions/authActions.ts
+++ frontend/src/redux/actions/authActions.ts
@@ -1,1 +1,10 @@
+import { login, logout } from '../reducers/authReducer';
+import { AppDispatch } from '../store';
 
+export const loginUser = () => (dispatch: AppDispatch) => {
+  dispatch(login());
+};
+
+export const logoutUser = () => (dispatch: AppDispatch) => {
+  dispatch(logout());
+};
