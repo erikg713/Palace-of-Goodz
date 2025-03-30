c4f5d090cfb955b0a3b39c7b1809dd950c381905
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-03-29 23:41:52 -0500

    Update store.ts

--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -1,1 +1,12 @@
+import { configureStore } from '@reduxjs/toolkit';
+import authReducer from './reducers/authReducer';
+import productReducer from './reducers/productReducer';
 
+const store = configureStore({
+  reducer: {
+    auth: authReducer,
+    products: productReducer,
+  },
+});
+
+export default store;
