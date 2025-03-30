09b1a9b7c777778ef2f9ce779bb875da724ac214
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-03-29 23:35:13 -0500

    Update productReducer.ts

--- frontend/src/redux/reducers/productReducer.ts
+++ frontend/src/redux/reducers/productReducer.ts
@@ -1,1 +1,17 @@
+import { createSlice } from '@reduxjs/toolkit';
 
+const productSlice = createSlice({
+  name: 'products',
+  initialState: [],
+  reducers: {
+    addProduct: (state, action) => {
+      state.push(action.payload);
+    },
+    removeProduct: (state, action) => {
+      return state.filter(product => product.id !== action.payload.id);
+    },
+  },
+});
+
+export const { addProduct, removeProduct } = productSlice.actions;
+export default productSlice.reducer;
