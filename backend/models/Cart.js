6b629d554d8ef839076d31a60191d2de6cac0a92
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-03-29 20:58:25 -0500

    Update and rename Carts.sql to Cart.js

---
+++ backend/models/Cart.js
@@ -0,0 +1,12 @@
+
+CREATE TABLE Carts (
+    id SERIAL PRIMARY KEY,
+    userId INTEGEimestamp of cart creation
+);
+
+-- Table to store products within carts
+CREATE userId);
+CREATE INDEX idx_cartProducts_cartId ON CartProducts (cartId);
+CREATE INDEX idx_cartProducts_productId ON CartProducts (productId);
+cart    
+CARTPRODUCTS
