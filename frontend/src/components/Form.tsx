312034d3de4ac90cb9878befbe5ecba566dddc17
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-03-30 00:04:04 -0500

    Update Form.tsx

--- frontend/src/components/Form.tsx
+++ frontend/src/components/Form.tsx
@@ -1,1 +1,20 @@
+import React from 'react';
+import './Form.css';
 
+const Form: React.FC = () => {
+  return (
+    <form>
+      <div className="form-group">
+        <label htmlFor="name">Name:</label>
+        <input type="text" id="name" name="name" />
+      </div>
+      <div className="form-group">
+        <label htmlFor="email">Email:</label>
+        <input type="email" id="email" name="email" />
+      </div>
+      <button type="submit" className="button">Submit</button>
+    </form>
+  );
+};
+
+export default Form;
