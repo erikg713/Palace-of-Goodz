47b7cf6487db6a012d36638beddc20d6dc59fbb4
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-04-11 18:07:23 -0500

    Create db.js
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

---
+++ backend/db.js
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose';
+
+export function connectToDatabase() {
+  mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('MongoDB connection error:', err));
+}
