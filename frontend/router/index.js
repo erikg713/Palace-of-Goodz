92a917bfc096ca45dcbd7adba2171376318b29e9
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-04-11 18:25:05 -0500

    Update index.js
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- frontend/router/index.js
+++ frontend/router/index.js
@@ -1,14 +1,3 @@
-// File: frontend/src/router/index.js
-// File: frontend/src/router/index.js import { createRouter, createWebHistory } from 'vue-router' import HomePage from '../pages/HomePage.vue'
-
-const routes = [ { path: '/', name: 'Home', component: HomePage } ]
-
-const router = createRouter({ history: createWebHistory(), routes })
-
-export default router
-
-
-
 import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from '../pages/HomePage.vue'
 import AdminPanel from '../pages/AdminPanel.vue'
