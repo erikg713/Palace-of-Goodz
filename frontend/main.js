21e36d8f82d8888e20bdd7bb7108c093857e40ab
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-04-11 19:38:03 -0500

    Update main.js
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- frontend/main.js
+++ frontend/main.js
@@ -2,5 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './style.css'
+script src="https://sdk.minepi.com/pi-sdk.js"></script>
+<script>
+  Pi.init({ version: "2.0", sandbox: true });
+</script>
 
 createApp(App).use(router).mount('#app')
