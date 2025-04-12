655269af05054a024470e27e8d31687fdfe70793
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-04-11 19:38:43 -0500

    Update index.js
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- frontend/router/index.js
+++ frontend/router/index.js
@@ -1,6 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from '../pages/HomePage.vue'
 import AdminPanel from '../pages/AdminPanel.vue'
+script src="https://sdk.minepi.com/pi-sdk.js"></script>
+<script>
+  Pi.init({ version: "2.0", sandbox: true });
+</script>
 
 const routes = [
   {
