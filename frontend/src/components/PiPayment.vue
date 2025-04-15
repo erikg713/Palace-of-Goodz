37b71601bd5cd45761a191d3f99ceed8ae360546
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-04-15 12:22:08 -0500

    Create PiPayment.vue
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

---
+++ frontend/src/components/PiPayment.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <button @click="pay">Pay 3.14π for Test Item</button>
+  </div>
+</template>
+
+<script setup>
+import { startPayment } from '@/pi/payments'
+const props = defineProps({ token: String })
+
+const pay = () => {
+  startPayment({
+    amount: 3.14,
+    memo: 'Test item purchase',
+    metadata: { productId: 'test001' }
+  }, props.token)
+}
+</script>
