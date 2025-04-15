ae818855f77965376c8bfe38b3b49cb19097fc2f
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-04-15 11:53:53 -0500

    Update verifyUser.js
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- backend/utils/verifyUser.js
+++ backend/utils/verifyUser.js
@@ -1,1 +1,12 @@
+import axios from 'axios'
 
+export const verifyUser = async (accessToken) => {
+  try {
+    const { data } = await axios.get('https://api.minepi.com/me', {
+      headers: { Authorization: `Bearer ${accessToken}` }
+    });
+    return data;
+  } catch {
+    return null;
+  }
+}
