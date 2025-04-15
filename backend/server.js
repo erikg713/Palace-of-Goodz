d1b374acecaa763458b9f17a91ec1ad716931a3b
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-04-15 12:05:06 -0500

    Update server.js
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,17 @@
 import express from 'express'
-import dotenv from 'dotenv'
 import cors from 'cors'
+import dotenv from 'dotenv'
 import paymentRoutes from './routes/paymentRoutes.js'
 
 dotenv.config()
-
 const app = express()
+
 app.use(cors())
 app.use(express.json())
 
 app.use('/payment', paymentRoutes)
 
 const PORT = 5000
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+app.listen(PORT, () => {
+  console.log(`Backend server running at http://localhost:${PORT}`)
+})
