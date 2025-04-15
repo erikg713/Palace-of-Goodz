70af11227b69fe36d63da3ff3cfd6d4d11b330c0
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-04-15 12:44:49 -0500

    Create Admin.vue
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

---
+++ frontend/src/views/Admin.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h2>Admin Panel</h2>
+    <ProductForm @product-added="fetchProducts" />
+    <ProductList />
+  </div>
+</template>
+
+<script setup>
+import ProductForm from '@/components/ProductForm.vue'
+import ProductList from '@/components/ProductList.vue'
+
+const fetchProducts = () => {
+  // handled inside ProductList
+}
+</script>
