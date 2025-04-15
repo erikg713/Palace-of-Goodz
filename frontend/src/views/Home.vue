4b163dea40ffb4f2bd20b6a22c8f133e3dd043a7
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-04-15 11:26:19 -0500

    Update Home.vue
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,1 +1,32 @@
+<script setup>
+import { authenticateUser } from '@/pi/sdk'
+import { startPayment } from '@/pi/payments'
 
+const authUser = async () => {
+  const auth = await authenticateUser((payment) => {
+    console.log('Resume incomplete payment:', payment);
+  });
+  console.log('Logged in Pi user:', auth.user.username);
+};
+
+const buyTestProduct = async () => {
+  await startPayment({
+    amount: 3.14,
+    memo: 'Test item in sandbox',
+    metadata: { itemId: 'sandbox001' }
+  }, {
+    onApproval: (paymentId) => console.log('Approved:', paymentId),
+    onComplete: (paymentId, txid) => console.log('Completed:', txid),
+    onCancel: (id) => console.log('Cancelled:', id),
+    onError: (err) => console.error('Error:', err),
+  });
+};
+</script>
+
+<template>
+  <div>
+    <h1>Palace of Goodz</h1>
+    <button @click="authUser">Login with Pi</button>
+    <button @click="buyTestProduct">Buy Test Product</button>
+  </div>
+</template>
