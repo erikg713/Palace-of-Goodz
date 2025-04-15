e837167897b80ad6e5b7e539dee3ffc633682c63
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-04-15 11:59:52 -0500

    Create PiLogin.vue
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

---
+++ frontend/src/components/PiLogin.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <button @click="login">Login with Pi</button>
+  </div>
+</template>
+
+<script setup>
+import { authenticateUser } from '@/pi/sdk'
+import { emit } from 'vue'
+
+const emit = defineEmits(['logged-in'])
+
+const login = async () => {
+  const auth = await authenticateUser((payment) => {
+    console.log('Resume payment:', payment)
+  })
+  if (auth) {
+    emit('logged-in', auth.accessToken)
+  }
+}
+</script>
