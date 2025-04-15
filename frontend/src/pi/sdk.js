5517af18a51edf14f6a20b9783491659c82391a7
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-04-15 12:00:38 -0500

    Update sdk.js
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- frontend/src/pi/sdk.js
+++ frontend/src/pi/sdk.js
@@ -1,10 +1,10 @@
 export async function authenticateUser(onIncompletePaymentFound) {
   try {
-    const scopes = ['username', 'payments'];
-    const auth = await Pi.authenticate(scopes, onIncompletePaymentFound);
-    return auth;
+    const scopes = ['username', 'payments']
+    const auth = await Pi.authenticate(scopes, onIncompletePaymentFound)
+    return auth
   } catch (error) {
-    console.error('Pi Auth failed:', error);
-    throw error;
+    console.error('Auth error:', error)
+    return null
   }
 }
