67085defd5e1c64c4ce3873d009129ceef8ff837
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-04-16 03:28:27 -0500

    Update useProducts.js
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- frontend/src/composables/useProducts.js
+++ frontend/src/composables/useProducts.js
@@ -1,1 +1,19 @@
+import { ref, onMounted } from 'vue';
+import { getProducts } from '@/services/productService';
+
+export default function useProducts() {
+  const products = ref([]);
+
+  const loadProducts = async () => {
+    const response = await getProducts();
+    products.value = response.data;
+  };
+
+  onMounted(loadProducts);
+
+  return {
+    products,
+    loadProducts
+  };
+}
 
