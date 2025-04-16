cae347244aa3ca714cb53bc841d4b525c1d04e75
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-04-16 11:26:59 -0500

    Update LoginForm.vue
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- frontend/src/modules/authentication/components/LoginForm.vue
+++ frontend/src/modules/authentication/components/LoginForm.vue
@@ -1,1 +1,11 @@
+// Authenticate the user, and get permission to request payments from them:
+const scopes = ['payments'];
 
+// Read more about this callback in the SDK reference:
+function onIncompletePaymentFound(payment) { /* ... */ };
+
+Pi.authenticate(scopes, onIncompletePaymentFound).then(function(auth) {
+  console.log(`Hi there! You're ready to make payments!`);
+}).catch(function(error) {
+  console.error(error);
+});
