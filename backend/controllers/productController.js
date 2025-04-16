e922ccfd46696e3421c21388c44ecca885962fd4
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-04-16 01:21:38 -0500

    Update productController.js
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,25 +1,3 @@
-import Product from '../models/productModel.js';
-
-export const getProducts = async (req, res) => {
-  try {
-    const products = await Product.find();
-    res.json(products);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
-
-export const createProduct = async (req, res) => {
-  try {
-    const product = new Product(req.body);
-    const savedProduct = await product.save();
-    res.status(201).json(savedProduct);
-  } catch (error) {
-    res.status(400).json({ message: error.message });
-  }
-};
-
-
 import Product from '../models/Product.js';
 
 // Admin check helper
