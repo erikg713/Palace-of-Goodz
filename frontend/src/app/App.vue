c33ce3f141cda740c287e5f5417d67009fd1e258
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-04-16 04:47:11 -0500

    Update App.vue
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- frontend/src/app/App.vue
+++ frontend/src/app/App.vue
@@ -1,3 +1,22 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+
+const app = createApp(App);
+
+app.config.globalProperties.$pi = {
+  init: () => {
+    return new Promise((resolve) => {
+      window.addEventListener('sdkReady', () => {
+        Pi.init({ version: "2.0", sandbox: true });
+        resolve();
+      });
+    });
+  }
+};
+
+app.mount('#app');
+
+
 <template>
   <router-view />
 </template>
