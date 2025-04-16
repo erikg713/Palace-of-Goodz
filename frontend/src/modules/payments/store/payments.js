e7ed5c1da6063d3a01a74157e1180809d1f62ad5
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-04-16 11:27:59 -0500

    Update payments.js
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- frontend/src/modules/payments/store/payments.js
+++ frontend/src/modules/payments/store/payments.js
@@ -1,1 +1,16 @@
 
+Pi.createPayment({
+  // Amount of π to be paid:
+  amount: 3.14,
+  // An explanation of the payment - will be shown to the user:
+  memo: "...", // e.g: "Digital kitten #1234",
+  // An arbitrary developer-provided metadata object - for your own usage:
+  metadata: { /* ... */ }, // e.g: { kittenId: 1234 }
+}, {
+  // Callbacks you need to implement - read more about those in the detailed docs linked below:
+  onReadyForServerApproval: function(paymentId) { /* ... */ },
+  onReadyForServerCompletion: function(paymentId, txid) { /* ... */ },
+  onCancel: function(paymentId) { /* ... */ },
+  onError: function(error, payment) { /* ... */ },
+});
+
