326625ed57db848976863a4b17141eb691741450
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-04-16 04:12:53 -0500

    Update ProductList.vue
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- frontend/src/modules/products/views/ProductList.vue
+++ frontend/src/modules/products/views/ProductList.vue
@@ -1,1 +1,21 @@
+<template>
+  <div class="grid gap-4">
+    <ProductCard
+      v-for="product in products"
+      :key="product._id"
+      :product="product"
+      @delete-product="deleteProduct"
+    />
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+import ProductCard from '../components/ProductCard.vue';
+import useProducts from '../composables/useProducts';
+
+const { products, loadProducts, deleteProduct } = useProducts();
+
+onMounted(loadProducts);
+</script>
 
