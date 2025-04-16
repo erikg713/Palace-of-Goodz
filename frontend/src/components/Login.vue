f2e04d60331612022566c08dd4126de5f40d3de7
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-04-16 18:04:48 -0500

    Update Login.vue
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -1,27 +1,12 @@
 <script setup>
-import { onMounted } from 'vue';
+import { piLogin } from '@/utils/piLogin';
+import axios from 'axios';
 
-function loginWithPi() {
-  const scopes = ['payments'];
-
-  Pi.authenticate(scopes, (payment) => {
-    console.log('Incomplete payment found:', payment);
-  }).then(auth => {
-    console.log('Pi Auth Success:', auth);
-
-    fetch('/api/auth/pi-login', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(auth),
-    })
-    .then(res => res.json())
-    .then(data => {
-      localStorage.setItem('token', data.token); // Save token for later
-    });
-  }).catch(console.error);
-}
+const login = async () => {
+  const user = await piLogin();
+  await axios.post('/api/auth/login', {
+    uid: user.uid,
+    username: user.username,
+  });
+};
 </script>
-
-<template>
-  <button @click="loginWithPi">Login with Pi</button>
-</template>
