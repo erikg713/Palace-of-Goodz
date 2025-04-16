85840799595efc3a48e916d62b84939706a5adfb
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-04-16 18:17:53 -0500

    Update piAuthMiddleware.js
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- backend/middlewares/piAuthMiddleware.js
+++ backend/middlewares/piAuthMiddleware.js
@@ -10,15 +10,3 @@ export const piAuthMiddleware = (req, res, next) => {
   req.user = { uid, username, role: role || 'customer' };
   next();
 };
-export const piAuthMiddleware = (req, res, next) => {
-  const uid = req.headers['x-user-id'];
-  const username = req.headers['x-user-username'];
-  const role = req.headers['x-user-role'];
-
-  if (!uid || !username) {
-    return res.status(401).json({ error: 'Unauthorized: Missing Pi credentials' });
-  }
-
-  req.user = { uid, username, role: role || 'customer' };
-  next();
-};
