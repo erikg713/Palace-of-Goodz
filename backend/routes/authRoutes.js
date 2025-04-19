6ffdc810af26166c707120724deaacd2f75d4184
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-04-18 23:34:36 -0500

    Update authRoutes.js
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,23 +1,8 @@
-import express from 'express';
-import { registerUser } from '../controllers/authController.js';
-import { registerValidation } from '../validators/authValidators.js';
-import { validationResult } from 'express-validator';
-const express = require('express');
-const router = express.Router();
-const verifyPiToken = require('../middlewares/piAuth');
+import express from 'express'
+import { verifyPiUser } from '../controllers/piAuthController.js'
 
-router.get('/profile', verifyPiToken, async (req, res) => {
-  const user = req.piUser;
-  res.json({ message: `Welcome, ${user.username}` });
-});
-const router = express.Router();
+const router = express.Router()
 
-router.post('/register', registerValidation, (req, res, next) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
-  }
-  registerUser(req, res, next);
-});
+router.post('/pi-login', verifyPiUser)
 
-export default router;
+export default router
