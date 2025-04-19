ecb4afac9437e0ad18b594f925815c90c4947108
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-04-18 23:11:30 -0500

    Update userRoutes.js
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -42,3 +42,13 @@ router.get('/', authorize, rateLimiter, async (req, res, next) => {
 });
 
 export default router;
+const express = require('express');
+const router = express.Router();
+const verifyPiToken = require('../middlewares/piAuth');
+
+router.get('/me', verifyPiToken, (req, res) => {
+  const user = req.piUser;
+  res.json({ message: `Welcome back, ${user.username}`, uid: user.uid });
+});
+
+module.exports = router;
