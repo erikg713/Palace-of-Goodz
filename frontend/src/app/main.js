dd1a227ba66270d53415a7606a09fe58a4da2adc
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-04-19 23:07:49 -0500

    Update main.js
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- frontend/src/app/main.js
+++ frontend/src/app/main.js
@@ -22,3 +22,22 @@ import App from './App.vue'
 import router from './router'
 
 createApp(App).use(router).mount('#app')
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+
+const app = createApp(App);
+
+app.config.globalProperties.$pi = {
+  init: () => {
+    return new Promise((resolve) => {
+      window.addEventListener('sdkReady', () => {
+        Pi.init({ version: "2.0", sandbox: true });
+        resolve();
+      });
+    });
+  }
+};
+
+app.use(router);
+app.mount('#app');
