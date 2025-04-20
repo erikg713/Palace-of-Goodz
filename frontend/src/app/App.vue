43af92a46f92454455f688c6939397974ed5bd98
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-04-19 23:01:19 -0500

    Update App.vue
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- frontend/src/app/App.vue
+++ frontend/src/app/App.vue
@@ -6,7 +6,91 @@ import userRoutes from "./routes/userRoutes";
 import orderRoutes from "./routes/orderRoutes";
 import paymentRoutes from "./routes/paymentRoutes";
 import { errorHandler } from "./utils/errorHandler";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
+import Navbar from "./components/Navbar";
+import PrivateRoute from "./components/PrivateRoute";
+import PrivateAdminRoute from "./components/PrivateAdminRoute";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import PrivateAdminRoute from "./routes/PrivateAdminRoute";
+import AdminDashboard from "./pages/AdminDashboard";
+import UserDashboard from "./pages/UserDashboard";
+import LoginPage from "./pages/LoginPage";
 
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/dashboard" element={<UserDashboard />} />
+        <Route
+          path="/admin"
+          element={
+            <PrivateAdminRoute>
+              <AdminDashboard />
+            </PrivateAdminRoute>
+          }
+        />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
+// Pages
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Dashboard from "./pages/Dashboard";
+import Checkout from "./pages/Checkout";
+import Orders from "./pages/Orders";
+import AdminOrders from "./pages/AdminOrders";
+import ProductDetails from "./pages/ProductDetails";
+import Cart from "./pages/Cart";
+import AdminDashboard from "./pages/AdminDashboard";
+import PiAuth from "./components/PiAuth";
+import PiPayment from "./components/PiPayment";
+
+const App = () => {
+  return (
+    <AuthProvider>
+      <Router>
+        <Navbar />
+        <Routes>
+          {/* Public Routes */}
+          <Route path="/" element={<Home />} />
+          <Route path="/product/:id" element={<ProductDetails />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+
+          {/* Protected User Routes */}
+          <Route path="/checkout" element={<PrivateRoute><Checkout /></PrivateRoute>} />
+          <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
+          <Route path="/orders" element={<PrivateRoute><Orders /></PrivateRoute>} />
+
+          {/* Protected Admin Routes */}
+          <Route path="/admin/orders" element={<PrivateAdminRoute><AdminOrders /></PrivateAdminRoute>} />
+          <Route path="/admin" element={<PrivateAdminRoute><AdminDashboard /></PrivateAdminRoute>} />
+
+          {/* Pi Network DApp Inside Dashboard */}
+          <Route path="/dashboard/pi" element={
+            <PrivateRoute>
+              <div>
+                <h1>Pi Network DApp</h1>
+                <PiAuth />
+                <PiPayment />
+              </div>
+            </PrivateRoute>
+          } />
+        </Routes>
+      </Router>
+    </AuthProvider>
+  );
+};
+
+export default App;
 const app = express();
 
 // ✅ Middleware
