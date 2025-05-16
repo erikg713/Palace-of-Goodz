6239e185a0ed08feb7115849f9ca0c489a38d0c0
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-05-16 03:15:15 -0500

    Update vue.config.js
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,24 +1,51 @@
+const { GenerateSW } = require('workbox-webpack-plugin')
+
 module.exports = {
   chainWebpack: (config) => {
-    // Environment-specific configurations
     config
       .when(process.env.NODE_ENV === 'development', (config) => {
-        config.devtool('source-map') // Enable source maps in development for easier debugging
+        config.devtool('source-map')
       })
       .when(process.env.NODE_ENV === 'production', (config) => {
-        config.optimization.minimize(true) // Minimize code for better performance in production
+        config.optimization.minimize(true)
       })
 
-    // Support custom elements with 'ion-' prefix
     config.module
       .rule('vue')
       .use('vue-loader')
       .tap((options) => ({
         ...options,
         compilerOptions: {
-          isCustomElement: (tag) => tag.startsWith('ion-') // Treat 'ion-' tags as custom elements
+          isCustomElement: (tag) => tag.startsWith('ion-')
         }
       }))
+  },
+
+  pwa: {
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      swSrc: 'src/service-worker.js', // your custom service worker
+      exclude: [/\.map$/, /_redirects/],
+    },
+    name: 'Palace of Goodz',
+    themeColor: '#1a1a1a',
+    manifestOptions: {
+      short_name: 'POG',
+      start_url: '.',
+      display: 'standalone',
+      background_color: '#1a1a1a',
+      icons: [
+        {
+          src: 'img/icons/android-chrome-192x192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: 'img/icons/android-chrome-512x512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        }
+      ]
+    }
   }
 }
-
