ef626366eee0352b5feda98200952a39f15e1b2d
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-08-18 20:08:18 -0500

    Update piPayments.js
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- backend/utils/piPayments.js
+++ backend/utils/piPayments.js
@@ -1,1 +1,19 @@
+import axios from "axios";
+import { PI_BASE_URL, PI_API_KEY } from "../config/pi.js";
 
+export async function createPayment(amount, memo, uid) {
+  const response = await axios.post(
+    `${PI_BASE_URL}/payments`,
+    { amount, memo, metadata: { uid } },
+    { headers: { Authorization: `Key ${PI_API_KEY}` } }
+  );
+  return response.data;
+}
+
+export async function verifyPayment(paymentId) {
+  const response = await axios.get(
+    `${PI_BASE_URL}/payments/${paymentId}`,
+    { headers: { Authorization: `Key ${PI_API_KEY}` } }
+  );
+  return response.data;
+}
