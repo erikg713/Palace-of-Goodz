f9e7b8273e11331b0ac113a13008e46049f91bef
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-08-18 19:14:04 -0500

    Update Product.js
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -1,4 +1,13 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  price_in_pi: { type: Number, required: true },
+  description: { type: String },
+  image_url: { type: String }
+}, { timestamps: true });
+
+export default mongoose.model("Product", productSchema);
 
 const productSchema = new mongoose.Schema({
   name: { type: String, required: true },
