640f36b6b68dbc247e93b8d27fa1919a4eb9a130
Dev713 <53387865+erikg713@users.noreply.github.com>
2026-04-13 13:07:02 -0500

    Update jwt.js
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- backend/config/jwt.js
+++ backend/config/jwt.js
@@ -1,1 +1,7 @@
+const jwtConfig = {
+  secret: process.env.JWT_SECRET,
+  accessTokenExpiresIn: process.env.JWT_ACCESS_EXPIRE || '15m',
+  refreshTokenExpiresIn: process.env.JWT_REFRESH_EXPIRE || '7d',
+};
 
+export default jwtConfig;
